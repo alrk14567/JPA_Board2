31660820caa4455134e759791223afda4da47652
alrk14567 <cdutik@gmail.com>
2024-08-02 13:59:37 +0900

    aslidhj

---
+++ src/test/java/com/nc13/JPA_Board2/JpaBoard2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nc13.JPA_Board2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaBoard2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
